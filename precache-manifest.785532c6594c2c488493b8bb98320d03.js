d639daf2f53d64bfaa6e8d8ad2981f5b49c99de1
Amr Fouad <68440701+3mrfouad@users.noreply.github.com>
2020-09-16 02:11:12 -0600

    Updates

---
+++ precache-manifest.785532c6594c2c488493b8bb98320d03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71ca91b6101861a1d5fb74c4263c765b",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "06e1f6d567057092a9e3",
+    "url": "/calculator/static/css/main.8ae27a67.chunk.css"
+  },
+  {
+    "revision": "d43635e74b477a826e5c",
+    "url": "/calculator/static/js/2.3dd7adeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.3dd7adeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06e1f6d567057092a9e3",
+    "url": "/calculator/static/js/main.32708a0c.chunk.js"
+  },
+  {
+    "revision": "68cc89dccfc5eefb31a9",
+    "url": "/calculator/static/js/runtime-main.6a6be215.js"
+  }
+]);
